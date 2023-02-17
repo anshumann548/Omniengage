d503bfa873770e2e28697cb8d5cdb8de2128a12b
anshumann548 <anshumann548@gmail.com>
2023-02-17 18:00:39 +0530

    Cross browser testing functionality added

--- src/test/java/omniengage/Login.java
+++ src/test/java/omniengage/Login.java
@@ -1,38 +1,40 @@
 
-	package omniengage;
+package omniengage;
 
+import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-	import org.openqa.selenium.By;
-	import org.openqa.selenium.WebDriver;
-	import org.openqa.selenium.chrome.ChromeDriver;
-	import org.openqa.selenium.support.ui.WebDriverWait;
-	import org.testng.annotations.Test;
+@Test
+public class Login {
+	WebDriver driver;
+	WebDriverWait wait;
 
-	public class Login extends Nodes{ 
-		WebDriver driver;
-		WebDriverWait wait;
+	
+@Test
+	public void Loginpage( WebDriver driver) throws InterruptedException {
+
+//		System.setProperty("webdriver.chrome.driver", "/home/anshumann/Downloads/chromedriver");
+		this.driver = driver;
+
+		this.driver.manage().window().maximize(); // maximizing window
+
+		this.driver.navigate().to("https://app.omniengage.co/login"); // navigating to url
+
+//		this.driver.manage().window().maximize(); // maximizing window
+
+		this.driver.findElement(By.xpath("//*[@id=\'root\']/div/div/div/div[2]/form/div/div[3]/div/input"))
 		
-		@Test
-		public void Loginpage() throws InterruptedException {
-			
-			System.setProperty("webdriver.chrome.driver", "/home/anshumann/Downloads/chromedriver");
-			driver = new ChromeDriver(); //initializing driver
+		.sendKeys("anshumann.singh@venturepact.com");
 		
-			
-			this.driver.navigate().to("https://app.omniengage.co/login"); //navigating to url
-			
-			this.driver.manage().window().maximize(); // maximizing window
-			
-			
-			this.driver.findElement(By.xpath("//*[@id=\'root\']/div/div/div/div[2]/form/div/div[3]/div/input")).sendKeys("anshumann.singh@venturepact.com");
-			this.driver.findElement(By.xpath("//*[@id=\'root\']/div/div/div/div[2]/form/div/div[4]/div[1]/input")).sendKeys("Pass@123");
-			//Sending user name and password
-			
-			this.driver.findElement(By.xpath("//*[@id=\'root\']/div/div/div/div[2]/form/div/div[5]/input")).click();
-			//Clicking submit
-		}
+		this.driver.findElement(By.xpath("//*[@id=\'root\']/div/div/div/div[2]/form/div/div[4]/div[1]/input"))
+		
+		.sendKeys("Pass@123");
+		// Sending user name and password
 
+		this.driver.findElement(By.xpath("//*[@id=\'root\']/div/div/div/div[2]/form/div/div[5]/input")).click();
+		// Clicking submit
 	}
 
-
-
+}
--- src/test/java/omniengage/Nodes.java
+++ src/test/java/omniengage/Nodes.java
@@ -2,11 +2,68 @@ package omniengage;
 
 import java.awt.AWTException;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 @Test
 public class Nodes {
+	WebDriver driver ;
+	@BeforeTest
+	@Parameters("browser")
+	public void setup(String browser) throws Exception {
+		
+		// Check if parameter passed from TestNG is 'firefox'
+		
+		if (browser.equalsIgnoreCase("firefox")) {
+			
+			// create firefox instance
+			
+			System.setProperty("webdriver.gecko.driver", "/home/anshumann/Downloads/geckodriver");
+			
+			this.driver = new FirefoxDriver(); // initializing driver
+		
+		} 
+		
+		// Check if parameter passed as 'chrome'
+		
+		else if (browser.equalsIgnoreCase("chrome")) {
+		
+			// set path to chromedriver.exe
+			
+			System.setProperty("webdriver.chrome.driver", "/home/anshumann/Downloads/chromedriver");
+			
+			// create chrome instance
+			
+			this.driver = new ChromeDriver();
+		}
 
+		// Check if parameter passed as 'Edge'
+		
+		else if (browser.equalsIgnoreCase("Edge")) {
+		
+			// set path to Edge.exe
+			
+			System.setProperty("webdriver.edge.driver", "/home/anshumann/Downloads/msedgedriver");
+			
+			// create Edge instance
+				this.driver = new EdgeDriver();
+		}
+		
+		else {
+			
+			// If no browser passed throw exception
+			
+			throw new Exception("Browser is not correct");
+		}
+		
+		Thread.sleep(5000);
+
+	}
 	public void controller() throws InterruptedException, AWTException {
 
 		Chatanalytics obj = new Chatanalytics();
@@ -14,7 +71,7 @@ public class Nodes {
 		// functions called,you have to call the last function at first to create
 		// inverse calling hierarchy
 
-		obj.Loginpage();
+		obj.Loginpage(this.driver);
 		obj.BotmakerNode();
 		obj.dragndropNode();
 		obj.inserter();
